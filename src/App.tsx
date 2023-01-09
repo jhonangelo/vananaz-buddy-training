60832ec213a1118878da0d800607e17530cfea0d
jhonangelo <jhon@vananaz.com>
2023-01-09 16:53:58 +0800

    Add textlink component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Button } from './components/atoms/Button';
 import { Input } from './components/atoms/Input';
 import { SearchInput } from './components/atoms/SearchInput';
+import { TextLink } from './components/atoms/TextLink';
 
 function App() {
   return (
@@ -10,9 +11,10 @@ function App() {
       <Button label={'Login'} buttonType='primary' />
       <Button label={'Select'} buttonType='secondary' />
       <Button label={'No'} buttonType='warning' />
-      <Input label={'Email'} inputType={'text'} />
-      <Input label={'Password'} inputType={'password'} clearOption />
+      <Input label={'Email'} type={'text'} />
+      <Input label={'Password'} type={'password'} hasClearButton />
       <SearchInput />
+      <TextLink text={'Add your first to do'} linkTo={'abc.com'} />
     </div>
   );
 }
