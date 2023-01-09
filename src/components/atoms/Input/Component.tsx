9200099739c4742b23ee8217725ea139d449d85f
jhonangelo <jhon@vananaz.com>
2023-01-09 12:36:51 +0800

    TRAIN-866 Add input component

---
+++ src/components/atoms/Input/Component.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Delete } from './Icons/delete.svg';
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledLabel = styled.label`
+  color: #828282;
+  font-weight: 400;
+  font-size: 14px;
+  text-align: left;
+`;
+
+const InputContainer = styled.div<Props>`
+  display: flex;
+  background: white;
+  width: 260px;
+  height: 32px;
+  margin-top: 11px;
+  border: solid 1px #828282;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: space-between;
+
+  &:focus-within {
+    outline: 2px solid #2f80ed;
+  }
+`;
+
+const TextInput = styled.input`
+  border: none;
+  border-radius: 4px;
+  font-size: 14px;
+  width: 100%;
+  margin-inline: 12px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const IconWrapper = styled.button`
+  display: flex;
+  width: 20px;
+  height: 20px;
+  align-items: center;
+  background-color: #828282;
+  border: 0;
+  border-radius: 50%;
+  cursor: pointer;
+  margin-right: 6px;
+`;
+
+export type Props = {
+  label?: string;
+  inputType?: string;
+  clearOption?: boolean;
+  onChange?: () => void;
+};
+
+const Component = ({
+  label,
+  inputType,
+  clearOption,
+}: Props): React.ReactElement => {
+  return (
+    <StyledDiv>
+      <StyledLabel>{label}</StyledLabel>
+      <InputContainer>
+        <TextInput type={inputType} />
+        {clearOption && (
+          <IconWrapper>
+            <Delete />
+          </IconWrapper>
+        )}
+      </InputContainer>
+    </StyledDiv>
+  );
+};
+
+export default Component;
---
+++ src/components/atoms/Input/Input.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { action } from '@storybook/addon-actions';
+
+import { Input } from '.';
+import { Props } from './Component';
+
+export default {
+  title: 'atoms/Input',
+  component: Input,
+  argTypes: {
+    inputType: {
+      options: ['text', 'password'],
+      control: { type: 'radio' },
+    },
+    clearOption: {
+      option: [true, false],
+      control: 'boolean',
+    },
+  },
+} as Meta;
+
+const Template: Story<Props> = (args: Props) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'Login',
+  inputType: 'text',
+  clearOption: false,
+  onChange: action('input-text-changed'),
+};
