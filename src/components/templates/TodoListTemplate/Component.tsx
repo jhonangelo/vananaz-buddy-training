0134cd58cfd1db638e60ec591e93dc826cbaab1c
jhonangelo <jhon@vananaz.com>
2023-01-18 11:23:55 +0800

    Add to do list template component

--- src/components/templates/TodoListTemplate/Component.tsx
+++ src/components/templates/TodoListTemplate/Component.tsx
@@ -4,8 +4,20 @@ import { ReactComponent as HomeButton } from './Icons/home.svg';
 import theme from '../../../constants/themes';
 import { SearchInput } from '../../atoms/SearchInput';
 import { Button } from '../../atoms/Button';
+import { RoundedIconBtn } from '../../atoms/RoundedIconBtn';
+import { ReactComponent as EmptyIcon } from './Icons/empty.svg';
+import { TextLink } from '../../atoms/TextLink';
+import { TodoList } from '../../organims/TodoList';
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  padding: 16px 16px 0px 16px;
+  height: 100vh;
+  box-sizing: border-box;
+`;
 
 const AppLogo = styled.h1`
   font-weight: 700;
@@ -14,28 +26,116 @@ const AppLogo = styled.h1`
   color: ${theme.colors.blue1}; ;
 `;
 
-const TemplateHeader = styled.div`
+const Header = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  row-gap: 9px;
   justify-content: space-between;
 `;
 
-const TemplateContent = styled.div``;
+const Controls = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 9px;
+`;
 
-type Props = {};
+const StyledSeachInput = styled(SearchInput)`
+  flex: 1;
+`;
 
-const Component = (props: Props) => {
+const EmptyContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const QuoteMessage = styled.p`
+  margin-top: 22px;
+  margin-bottom: 13px;
+`;
+
+const TodoListContainer = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const TodoItemListWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  overflow-y: scroll;
+  color: ${theme.colors.gray3};
+`;
+
+const ButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 21px;
+  right: 24px;
+`;
+
+interface Todo {
+  id: number;
+  text: string;
+  isDone: boolean;
+}
+
+export type Props = {
+  data?: Todo[];
+  SearchInputClick: () => void;
+  SearchBtnClick: () => void;
+  itemClick: () => void;
+  handleUpdate: () => void;
+  handleDelete: () => void;
+  AddTodoBtnClick: () => void;
+};
+
+const Component = ({
+  data = [],
+  SearchInputClick,
+  SearchBtnClick,
+  itemClick,
+  handleUpdate,
+  handleDelete,
+  AddTodoBtnClick,
+}: Props) => {
   return (
     <Container>
-      <TemplateHeader style={{ display: 'flex', flexDirection: 'row' }}>
+      <Header>
         <AppLogo>ToDoish</AppLogo>
         <HomeButton />
-      </TemplateHeader>
-      <TemplateContent>
-        <SearchInput />
-        <Button label='Search' buttonType='secondary' />
-      </TemplateContent>
+      </Header>
+      <Controls>
+        <StyledSeachInput onClick={SearchInputClick} />
+        <Button
+          label='Search'
+          buttonType='secondary'
+          onClick={SearchBtnClick}
+        />
+      </Controls>
+      <TodoItemListWrapper>
+        {data?.length > 0 ? (
+          <TodoListContainer>
+            <TodoList
+              data={data}
+              itemClick={itemClick}
+              handleUpdate={handleUpdate}
+              handleDelete={handleDelete}
+            />
+          </TodoListContainer>
+        ) : (
+          <EmptyContainer>
+            <EmptyIcon />
+            <QuoteMessage>To do list increases productivity</QuoteMessage>
+            <TextLink text='Add your first to do' />
+          </EmptyContainer>
+        )}
+      </TodoItemListWrapper>
+      <ButtonWrapper>
+        <RoundedIconBtn onClick={AddTodoBtnClick} />
+      </ButtonWrapper>
     </Container>
   );
 };
---
+++ src/components/templates/TodoListTemplate/TodoListTemplate.stories.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { TodoListTemplate } from '.';
+import { Props } from './Component';
+import GlobalStyles from '../../../constants/themes/globalStyling';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'templates/TodoList',
+  component: TodoListTemplate,
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'mobile1',
+    },
+  },
+
+  argTypes: {
+    data: {
+      control: {
+        type: 'array',
+        of: [
+          {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'number',
+              },
+              text: {
+                type: 'string',
+              },
+              isDone: {
+                type: 'boolean',
+              },
+            },
+          },
+        ],
+      },
+    },
+  },
+} as Meta;
+
+const Template: Story<Props> = (args: Props) => {
+  return (
+    <>
+      <GlobalStyles />
+      <TodoListTemplate {...args} />
+    </>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  data: [{ id: 1, text: 'Get parcel from FBI', isDone: false }],
+  SearchInputClick: action('search-list-navigate'),
+  SearchBtnClick: action('search-button-navigate'),
+  itemClick: action('todo-item-click'),
+  handleUpdate: action('update-click'),
+  handleDelete: action('delete-click'),
+  AddTodoBtnClick: action('new-todo-button-click'),
+};
