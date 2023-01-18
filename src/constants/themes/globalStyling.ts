11a11c991d3f18e37171bf011cb52aebac3fac0c
jhonangelo <jhon@vananaz.com>
2023-01-18 09:28:16 +0800

    Fix global stylings

--- src/constants/themes/globalStyling.ts
+++ src/constants/themes/globalStyling.ts
@@ -1,16 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-
-  * {
+  *,
+  *::before,
+  *::after {
     margin: 0;
     padding: 0;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
+    box-sizing: inherit;
   }
-
   body {
+    box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
     font-style: normal;
   }
