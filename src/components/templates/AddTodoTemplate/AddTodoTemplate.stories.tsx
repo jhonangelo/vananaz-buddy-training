cc46876383e91f9e71546cdcc169437de8c4e790
jhonangelo <jhon@vananaz.com>
2023-01-19 15:07:08 +0800

    Add add todo list template component

---
+++ src/components/templates/AddTodoTemplate/AddTodoTemplate.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { AddTodoTemplate } from '.';
+import { Props } from './Component';
+import GlobalStyles from '../../../constants/themes/globalStyling';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'templates/AddTodo',
+  component: AddTodoTemplate,
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'mobile1',
+    },
+  },
+} as Meta;
+
+const Template: Story<Props> = (args: Props) => {
+  return (
+    <>
+      <GlobalStyles />
+      <AddTodoTemplate {...args} />
+    </>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  backBtnClick: action('go-back'),
+  formSubmit: action('add-todo-input'),
+};
