b5c2cfe04561ea81b85e60804ebbed2a8c1f013b
jhonangelo <jhon@vananaz.com>
2023-01-20 11:44:14 +0800

    Add update to do template

---
+++ src/components/templates/UpdateTodoTemplate/Component.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { BackButton } from '../../atoms/BackButton';
+import { Input } from '../../atoms/Input';
+import { ToastNotification } from '../../atoms/ToastNotification';
+import { showToast } from '../../atoms/ToastNotification/Component';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  height: 100vh;
+  row-gap: 20px;
+`;
+
+export type Props = {
+  backBtnClick: () => void;
+  updateSubmit: (input: string) => void;
+  currentValue: string;
+};
+
+const Component = ({ backBtnClick, updateSubmit, currentValue }: Props) => {
+  const [toUpdateValue, setUpdatedValue] = useState(currentValue);
+
+  const handleUpdateSubmit = (input: string) => {
+    updateSubmit(input);
+    showToast('To do updated');
+  };
+  return (
+    <Container>
+      <BackButton label='Update to do' backBtnClick={backBtnClick} />
+      <Input
+        formSubmit={handleUpdateSubmit}
+        currentValue={toUpdateValue}
+        hasClearButton
+        setUpdatedValue={setUpdatedValue}
+      />
+      <ToastNotification position='bottom-center' />
+    </Container>
+  );
+};
+
+export default Component;
---
+++ src/components/templates/UpdateTodoTemplate/index.ts
@@ -0,0 +1,3 @@
+import UpdateTodoTemplate from './Component';
+
+export { UpdateTodoTemplate };
