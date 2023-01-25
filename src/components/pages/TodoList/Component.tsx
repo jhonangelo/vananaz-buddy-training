5420c7c5b787e1a93b8a67d92be2194f94508e33
jhonangelo <jhon@vananaz.com>
2023-01-25 11:11:38 +0800

    Add the modal with options when deleting an item

--- src/components/pages/TodoList/Component.tsx
+++ src/components/pages/TodoList/Component.tsx
@@ -1,12 +1,17 @@
-import React, { useContext, useCallback } from 'react';
+import React, { useContext, useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TodoListTemplate } from '../../templates/TodoListTemplate';
 import { TodoContextType, TodoContext } from '../../../hooks/todos/hooks';
+import { DeleteModal } from '../../molecule/DeleteModal';
+import { showToast } from '../../atoms/ToastNotification/Component';
+import { ToastNotification } from '../../atoms/ToastNotification';
 
 type Props = {};
 
 const Component = (props: Props) => {
   const { todos, deleteTodo } = useContext<TodoContextType>(TodoContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentID, setCurrentID] = useState(0);
 
   const navigate = useNavigate();
   const AddTodoBtnClick = useCallback(
@@ -14,18 +19,43 @@ const Component = (props: Props) => {
     [navigate]
   );
 
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleDelete = (id: number) => {
+    setCurrentID(id);
+    setIsOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
+    closeModal();
+    deleteTodo(currentID);
+    setCurrentID(0);
+    showToast('To do deleted');
+  };
+
   return (
-    <TodoListTemplate
-      data={todos}
-      homeBtnClick={() => console.log('logout')}
-      SearchInputClick={() => alert('w')}
-      itemClick={() => console.log('todo-item-clicked')}
-      AddTodoBtnClick={AddTodoBtnClick}
-      handleDelete={deleteTodo}
-      handleUpdate={() => console.log('handle-update')}
-      SearchBtnClick={() => console.log('search-button')}
-      linkTo='/'
-    />
+    <>
+      <TodoListTemplate
+        data={todos}
+        homeBtnClick={() => console.log('logout')}
+        SearchInputClick={() => alert('w')}
+        itemClick={() => console.log('todo-item-clicked')}
+        AddTodoBtnClick={AddTodoBtnClick}
+        handleDelete={handleDelete}
+        handleUpdate={() => console.log('handle-update')}
+        SearchBtnClick={() => console.log('search-button')}
+        linkTo='/'
+      />
+      <ToastNotification duration={1500} position='bottom-center' />
+      <DeleteModal
+        isOpen={isOpen}
+        closeModal={closeModal}
+        noBtnClick={closeModal}
+        yesBtnClick={handleConfirmDelete}
+      />
+    </>
   );
 };
 
