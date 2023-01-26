485f794c1336739fa26f66b310e5bf44688aafea
jhonangelo <jhon@vananaz.com>
2023-01-26 17:35:44 +0800

    remove unused onchange

--- src/components/organisms/RegisterForm/Component.tsx
+++ src/components/organisms/RegisterForm/Component.tsx
@@ -6,21 +6,20 @@ import { Input } from '../../atoms/Input';
 const Container = styled.form`
   display: flex;
   flex-direction: column;
-  row-gap: 26px;
+  row-gap: 16px;
 `;
 
-type Props = {
+export type Props = {
   submitRegister: () => void;
-  onChange: () => void;
 };
 
 const Component = ({ submitRegister }: Props) => {
   return (
     <Container onSubmit={submitRegister}>
-      <Input label='Name' type='text' onChange={onChange} />
-      <Input label='Email' type='email' onChange={onChange} />
-      <Input label='Age' type='number' onChange={onChange} />
-      <Input label='Password' type='password' onChange={onChange} />
+      <Input label='Name' type='text' />
+      <Input label='Email' type='email' />
+      <Input label='Age' type='number' />
+      <Input label='Password' type='password' />
       <Button type='submit' label='Register' buttonType='primary' />
     </Container>
   );
